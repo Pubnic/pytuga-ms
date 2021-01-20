883feb028b6828919177c6d4b939e76910d91635
Vitor Nere <vitor.nere@hotmail.com>
2021-01-19 23:57:22 -0300

    Running config

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i -e "s/microservice/$1/g" serverless.yml package.json docker-compose.yml
+sed -i -e "s/template/$1/g" serverless.yml package.json docker-compose.yml
 rm serverless.yml-e package.json-e docker-compose.yml-e
